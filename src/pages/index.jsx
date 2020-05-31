60effd0d35b66de88efeb851830cd667cb6de950
Aleksandr Sidorenko <asidorenko@simdaq.com>
2020-05-31 15:16:20 +0300

    Persist List Reordering

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,23 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { DragDropContext } from 'react-beautiful-dnd';
-import data from '../initial-data.json';
+import initialData from '../initial-data.json';
 import Column from '../components/column';
 
-const IndexPage = () =>
-  data['column-order'].map((columnId) => {
+const IndexPage = () => {
+  const [data, setData] = useState(initialData);
+
+  return data['column-order'].map((columnId) => {
     const column = data.columns[columnId];
     const tasks = column.taskIds.map((taskId) => data.tasks[taskId]);
+    const onDragEnd = (result) => {
+      const { source, destination, draggableId } = result;
+      if (!destination) {
+        return;
+      }
+      const noChange =
+        destination.droppableId === source.droppableId &&
+        destination.index === source.index;
+      if (noChange) {
+        return;
+      }
+
+      setData((prev) => {
+        const sourceColumn = prev.columns[source.droppableId];
+        const newTaskIds = Array.from(sourceColumn.taskIds);
+        newTaskIds.splice(source.index, 1);
+        newTaskIds.splice(destination.index, 0, draggableId);
+        const newColumn = { ...sourceColumn, taskIds: newTaskIds };
+
+        return {
+          ...prev,
+          columns: { ...prev.columns, [newColumn.id]: newColumn },
+        };
+      });
+    };
 
     return (
-      <DragDropContext
-        key={column.id}
-        onDragEnd={() => {
-          // TODO Implement
-        }}
-      >
+      <DragDropContext key={column.id} onDragEnd={onDragEnd}>
         <Column column={column} tasks={tasks} />
       </DragDropContext>
     );
   });
+};
 
 export default IndexPage;
