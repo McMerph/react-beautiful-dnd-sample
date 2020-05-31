524185a4d1027546d3cd1d51e962c4426fc48e1e
Aleksandr Sidorenko <asidorenko@simdaq.com>
2020-05-31 23:10:59 +0300

    Conditionally prevent drag

--- src/components/task.jsx
+++ src/components/task.jsx
@@ -3,31 +3,45 @@ import PropTypes from 'prop-types';
 import { Draggable } from 'react-beautiful-dnd';
 import styled from 'styled-components';
 
+const getBackgroundColor = (props) => {
+  if (props.isDragDisabled) return 'lightgrey';
+  return props.isDragging ? 'lightgreen' : 'white';
+};
+
 const Text = styled.p`
   margin: 0 0 8px 0;
   padding: 8px;
   border: 1px solid lightgrey;
   border-radius: 2px;
-  background-color: ${(props) => (props.isDragging ? 'lightgreen' : 'white')};
+  background-color: ${getBackgroundColor};
   user-select: none;
 `;
 
-const Task = ({ task, index }) => (
-  <Draggable draggableId={task.id} index={index}>
-    {(provided, snapshot) => (
-      <Text
-        // eslint-disable-next-line react/jsx-props-no-spreading
-        {...provided.draggableProps}
-        // eslint-disable-next-line react/jsx-props-no-spreading
-        {...provided.dragHandleProps}
-        ref={provided.innerRef}
-        isDragging={snapshot.isDragging}
-      >
-        {task.content}
-      </Text>
-    )}
-  </Draggable>
-);
+const Task = ({ task, index }) => {
+  const isDragDisabled = task.id === 'task-1';
+
+  return (
+    <Draggable
+      draggableId={task.id}
+      index={index}
+      isDragDisabled={isDragDisabled}
+    >
+      {(provided, snapshot) => (
+        <Text
+          // eslint-disable-next-line react/jsx-props-no-spreading
+          {...provided.draggableProps}
+          // eslint-disable-next-line react/jsx-props-no-spreading
+          {...provided.dragHandleProps}
+          ref={provided.innerRef}
+          isDragging={snapshot.isDragging}
+          isDragDisabled={isDragDisabled}
+        >
+          {task.content}
+        </Text>
+      )}
+    </Draggable>
+  );
+};
 
 Task.propTypes = {
   task: PropTypes.exact({
