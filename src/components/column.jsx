246b99be0be09d6e61c561ff7a927bae333ce9a2
Aleksandr Sidorenko <asidorenko@simdaq.com>
2020-05-31 18:17:53 +0300

    Move Items between Columns

--- src/components/column.jsx
+++ src/components/column.jsx
@@ -8,6 +8,10 @@ const Wrapper = styled.div`
   margin: 8px;
   border: 1px solid lightgrey;
   border-radius: 2px;
+  flex: 0 0 220px;
+
+  display: flex;
+  flex-direction: column;
 `;
 const Title = styled.h3`
   margin: 0;
@@ -18,6 +22,8 @@ const TaskList = styled.div`
   padding: 8px;
   transition: background-color 200ms ease;
   background-color: ${(props) => (props.isDraggingOver ? 'skyblue' : '#fff')};
+  flex-grow: 1;
+  min-height: 100px;
 `;
 
 const Column = ({ column, tasks }) => (
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,34 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
 import { DragDropContext } from 'react-beautiful-dnd';
 import initialData from '../initial-data.json';
 import Column from '../components/column';
+
+const Wrapper = styled.div`
+  display: flex;
+`;
+
+const moveTaskInsideColumn = (column, startIndex, finishIndex, toMove) => {
+  const newTaskIds = Array.from(column.taskIds);
+  newTaskIds.splice(startIndex, 1);
+  newTaskIds.splice(finishIndex, 0, toMove);
+
+  return { ...column, taskIds: newTaskIds };
+};
+
+const removeTaskFromColumn = (column, index) => {
+  const newTaskIds = Array.from(column.taskIds);
+  newTaskIds.splice(index, 1);
+
+  return { ...column, taskIds: newTaskIds };
+};
+
+const addTaskToColumn = (column, index, toAdd) => {
+  const newTaskIds = Array.from(column.taskIds);
+  newTaskIds.splice(index, 0, toAdd);
+
+  return { ...column, taskIds: newTaskIds };
+};
 
 const IndexPage = () => {
   const [data, setData] = useState(initialData);
@@ -18,26 +45,44 @@ const IndexPage = () => {
     }
 
     setData((prev) => {
-      const sourceColumn = prev.columns[source.droppableId];
-      const newTaskIds = Array.from(sourceColumn.taskIds);
-      newTaskIds.splice(source.index, 1);
-      newTaskIds.splice(destination.index, 0, draggableId);
-      const newColumn = { ...sourceColumn, taskIds: newTaskIds };
+      const start = prev.columns[source.droppableId];
+      const finish = prev.columns[destination.droppableId];
+      if (start === finish) {
+        const newColumn = moveTaskInsideColumn(
+          start,
+          source.index,
+          destination.index,
+          draggableId
+        );
 
+        return {
+          ...prev,
+          columns: { ...prev.columns, [newColumn.id]: newColumn },
+        };
+      }
+
+      const newStart = removeTaskFromColumn(start, source.index);
+      const newFinish = addTaskToColumn(finish, destination.index, draggableId);
       return {
         ...prev,
-        columns: { ...prev.columns, [newColumn.id]: newColumn },
+        columns: {
+          ...prev.columns,
+          [newStart.id]: newStart,
+          [newFinish.id]: newFinish,
+        },
       };
     });
   };
 
   return (
     <DragDropContext onDragEnd={onDragEnd}>
-      {data['column-order'].map((columnId) => {
-        const column = data.columns[columnId];
-        const tasks = column.taskIds.map((taskId) => data.tasks[taskId]);
-        return <Column key={column.id} column={column} tasks={tasks} />;
-      })}
+      <Wrapper>
+        {data['column-order'].map((columnId) => {
+          const column = data.columns[columnId];
+          const tasks = column.taskIds.map((taskId) => data.tasks[taskId]);
+          return <Column key={column.id} column={column} tasks={tasks} />;
+        })}
+      </Wrapper>
     </DragDropContext>
   );
 };
